c5ea94e4a4c7cee39342d335cf5ac6a53a4a6ddd
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-12-17 08:00:17 -0300

    Create project structure

---
+++ src/main/java/br/com/rodrigofaria/awsrdssample/AwsRdsSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigofaria.awsrdssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigofaria/awsrdssample/AwsRdsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigofaria.awsrdssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRdsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
